53e77d4d561d28ca8a6c054d14738dd5aed2334a
EJAyarma <johnayarmahe@gmail.com>
2022-03-11 11:41:06 -0800

    Add task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point of program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(str);
+	return (0);
+}
