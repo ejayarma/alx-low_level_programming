d5787f7c37879cb3c0c1527fa1852f8b159b471c
EJAyarma <johnayarmahe@gmail.com>
2022-03-11 11:36:46 -0800

    Correct task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - Entry point of program
+ * Return: Returns 0
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle\n";
