4bf2b6dc0dfe3ac5dc0118004ee61814778dc4e8
EJAyarma <johnayarmahe@gmail.com>
2022-03-11 11:37:54 -0800

    Correct task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
