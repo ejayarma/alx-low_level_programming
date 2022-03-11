4731aa20e4bb61a62e15902088e8a0c3842c3813
EJAyarma <johnayarmahe@gmail.com>
2022-03-11 11:43:29 -0800

    Correct task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
-
-	printf(str);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
