7f1993e5723af12a73562ccdb9da58af7c00aa23
EJAyarma <johnayarmahe@gmail.com>
2022-03-13 08:12:05 -0700

    Correct task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry for the program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	short i;
+	for(i = 97; i < 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
