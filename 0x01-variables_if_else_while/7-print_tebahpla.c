590c2b3d4272119364c2e71fea08274dbd763569
EJAyarma <johnayarmahe@gmail.com>
2022-03-13 09:13:17 -0700

    Add task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry for the program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	unsigned int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
