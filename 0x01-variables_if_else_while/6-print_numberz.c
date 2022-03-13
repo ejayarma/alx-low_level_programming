d3bada2bac6726046a5000d8a8c822372c50bcaf
EJAyarma <johnayarmahe@gmail.com>
2022-03-13 08:51:59 -0700

    Add task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry for the program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	short i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
