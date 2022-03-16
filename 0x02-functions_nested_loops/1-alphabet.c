b0e31c395f3bdb75953eb7fb23bd5168a1d19533
EJAyarma <johnayarmahe@gmail.com>
2022-03-15 17:10:42 -0700

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Main entry for the program
+ * Check the code to see what it does
+ * Return: Returns 0 always
+ */
+int main(void)
+{
+	short i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
