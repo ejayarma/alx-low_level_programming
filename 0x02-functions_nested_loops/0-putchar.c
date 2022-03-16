fed3d136d8d83ca27c958d3da00ce51a72f3519c
EJAyarma <johnayarmahe@gmail.com>
2022-03-15 17:01:05 -0700

    Attempt task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Main entry for the program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
