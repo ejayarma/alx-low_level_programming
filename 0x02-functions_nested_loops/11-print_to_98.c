bbfcd427ca3e28dc412257fda3067b75ec1999b0
Ayarmz <johnayarmahe@gmail.com>
2022-03-22 16:26:46 +0000

    Correct task 0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,18 @@
 
 void print_to_98(int n)
 {
-int i;
+    int i;
 
-if (n > 98)
-{
-for (i = n; i > 98; --i)
-printf("%d, ", i);
-printf("98\n");
-}
-else
-{
-for (i = n; i < 98; ++i)
-printf("%d, ", i);
-printf("98\n");
-}
+    if (n > 98)
+    {
+        for (i = n; i > 98; --i)
+            printf("%d, ", i);
+        printf("98\n");
+    }
+    else
+    {
+        for (i = n; i < 98; ++i)
+            printf("%d, ", i);
+        printf("98\n");
+    }
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,9 +5,9 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98
- * @*n: pointer to the number
+ * @n: pointer to the number
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+    *n = 98;
 }
