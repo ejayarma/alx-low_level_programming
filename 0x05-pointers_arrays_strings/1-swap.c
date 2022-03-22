85fa719a81eb3d56eff6e9ab756d47b0b6a8bde2
Ayarmz <johnayarmahe@gmail.com>
2022-03-22 18:01:10 +0000

    Convert spaces to tabs

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,8 +10,8 @@
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
-    temp = *b;
-    *b = *a;
-    *a = temp;
+	int temp;
+	temp = *b;
+	*b = *a;
+	*a = temp;
 }
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -7,25 +7,25 @@
 
 void test_task_0(void)
 {
-    int *k;
-    int j;
-    k = &j;
-    j = 10;
-    printf("j before reset_to_98: %d\n", j);
-    reset_to_98(k);
-    printf("j after reset_to_98: %d\n", j);
+	int *k;
+	int j;
+	k = &j;
+	j = 10;
+	printf("j before reset_to_98: %d\n", j);
+	reset_to_98(k);
+	printf("j after reset_to_98: %d\n", j);
 }
 
 void test_task_1(void)
 {
-    int *k, *j;
-    int m = 10, n = 20;
+	int *k, *j;
+	int m = 10, n = 20;
 
-    k = &m;
-    j = &n;
-    printf("m, n before reset_to_98: %d, %d\n", m, n);
-    swap_int(k, j);
-    printf("m, n before reset_to_98: %d, %d\n", m, n);
+	k = &m;
+	j = &n;
+	printf("m, n before reset_to_98: %d, %d\n", m, n);
+	swap_int(k, j);
+	printf("m, n before reset_to_98: %d, %d\n", m, n);
 }
 
 /*
@@ -34,5 +34,5 @@ void test_task_1(void)
  */
 int main(void)
 {
-    test_task_1();
+	test_task_1();
 }
