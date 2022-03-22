0a35b80e5cb7af8ead8c607a2d96af4a5f8dc95f
Ayarmz <johnayarmahe@gmail.com>
2022-03-22 22:45:14 +0000

    Correct imports for task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string to stdout
- * @str: pointer to string
+ * _puts - Prints a string
+ * @str: The string to print
+ *
+ * Return: void
  */
 void _puts(char *str)
 {
-	int i;
-	char c;
-
-	i = 0;
-	c = *(str + i);
-	while (c != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(c);
-		i += 1;
-		c = *(str + i);
+		_putchar(*str);
 	}
+
 	_putchar('\n');
 }
