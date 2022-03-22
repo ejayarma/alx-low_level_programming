4f5689ae5abecf4a7d779ba928a4ebe4cbdee5ca
Ayarmz <johnayarmahe@gmail.com>
2022-03-22 17:56:40 +0000

    Correct task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,16 +3,15 @@
 #include "main.h"
 
 /**
- * swap_int - a function that swaps 
+ * swap_int - a function that swaps
  * the value of two integers
  * @n: first number
  * @b: second to the number
  */
 void swap_int(int *a, int *b)
 {
-int temp;
-temp = *a;
-*b = *a;
-*a = temp;
+    int temp;
+    temp = *b;
+    *b = *a;
+    *a = temp;
 }
-
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -3,6 +3,7 @@
 #include "_putchar.c"
 #include "main.h"
 #include "0-reset_to_98.c"
+#include "1-swap.c"
 
 void test_task_0(void)
 {
@@ -15,11 +16,23 @@ void test_task_0(void)
     printf("j after reset_to_98: %d\n", j);
 }
 
+void test_task_1(void)
+{
+    int *k, *j;
+    int m = 10, n = 20;
+
+    k = &m;
+    j = &n;
+    printf("m, n before reset_to_98: %d, %d\n", m, n);
+    swap_int(k, j);
+    printf("m, n before reset_to_98: %d, %d\n", m, n);
+}
+
 /*
  * main - Entry point of program
  * Description: For testing other programs in this project
  */
 int main(void)
 {
-    test_task_0();
+    test_task_1();
 }
