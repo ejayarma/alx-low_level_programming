1c5ee97d4dd202bcce38595acc3a344745383b7a
Ayarmz <johnayarmahe@gmail.com>
2022-03-23 02:55:14 +0000

    Add tasks 8 and 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,15 +11,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	for (;;)
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
-		i += 1;
-		if (*(src + i) == '\0')
-		{
-			break;
-		}
+		dest[i] = src[i];
 	}
-	return dest;
+	dest[i] = '\0';
+	return (dest);
 }
