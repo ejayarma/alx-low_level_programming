52a12cedb850fb102ffce4b2757b3cfcb3e558c8
Ayarmz <johnayarmahe@gmail.com>
2022-03-23 01:13:20 +0000

    Add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * rev_string - a function that  reverses a string.
+ * @s: pointer to string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+
+	char s2[i + 1];
+	i = 0;
+
+	while (*(s + i))
+	{
+		*(s2 + i) = *(s + i);
+		i += 1;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(s2 + j);
+		i -= 1;
+		j += 1;
+	}
+}
