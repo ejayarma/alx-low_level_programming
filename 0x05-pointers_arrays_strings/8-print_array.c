7a8a616d9461c039bd375dae079002d3b75eb586
Ayarmz <johnayarmahe@gmail.com>
2022-03-23 02:43:35 +0000

    Add tasks 8 and 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +11,8 @@
  */
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
 		{
