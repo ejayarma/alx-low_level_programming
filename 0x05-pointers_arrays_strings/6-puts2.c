c974fa78d8ac1df01d25b2e8ad8a50d0ec15e2d9
Ayarmz <johnayarmahe@gmail.com>
2022-03-23 01:47:35 +0000

    Add task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * puts2 - a function that  prints even-indexed character of a string,
- * starting with the first character, followed by a new line.
- * @str: pointer to string
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
+ *
  * Return: void
  */
 void puts2(char *str)
 {
-	int i = 0, j;
+	int i;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-		i += 1;
+		j++;
 	}
 
-	for (j = 0; j < i; j += 2)
+	for (i = 0; i < j; i += 2)
 	{
-		_putchar(j);
+		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
