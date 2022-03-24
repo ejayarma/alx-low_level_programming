5aa6c2316573b9e84fe4cee9ffa05cbb5b19f317
Ayarmz <johnayarmahe@gmail.com>
2022-03-24 23:17:25 +0000

    Correct task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,8 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	size_t dest_len = strlen(dest);
-	size_t i;
+	int dest_len = strlen(dest);
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
