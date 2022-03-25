cfc77da92456e7b4ef72c80bc7f39f6233c93ead
Ayarmz <johnayarmahe@gmail.com>
2022-03-25 01:30:18 +0000

    Add correction to task  7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,19 +8,19 @@
 
 char *leet(char *s)
 {
-    int i;
-    int j;
-    char l[] = "ol_ea__t";
+	int i;
+	int j;
+	char l[] = "ol_ea__t";
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; l[j] != '\0'; j++)
-        {
-            if (s[i] == l[j] || s[i] == (l[j] - 32))
-            {
-                s[i] = j + '0';
-            }
-        }
-    }
-    return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
 }
