5074c1c3dba573420408152b35316246300b94f5
EJAyarma <johnayarmahe@gmail.com>
2022-04-02 01:38:32 +0000

    Correct task 0 and task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,11 @@
 /**
  * @brief Displays the name of the program
  * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,8 +4,11 @@
 /**
  * @brief Displays the number of arguments passed to the program
  * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
  */
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
+	return (0);
 }
