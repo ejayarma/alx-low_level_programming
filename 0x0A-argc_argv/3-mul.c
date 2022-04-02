5ae0ab3410425c8578b6af0bd350d526dbe1f71d
EJAyarma <johnayarmahe@gmail.com>
2022-04-02 02:03:10 +0000

    Correct task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-    int x, y, product;
+	int x, y, product;
 
-    if (argc - 1 != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
-    product = x * y;
-
-    printf("%d\n", product);
-    return (0);
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	product = x * y;
+	printf("%d\n", product);
+	return (0);
 }
