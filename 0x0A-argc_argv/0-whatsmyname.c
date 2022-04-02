0c6c58ca1974e95e856ef111feafbf6022d1f537
EJAyarma <johnayarmahe@gmail.com>
2022-04-02 01:23:54 +0000

    Add task 0 and 1 with corrections included

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Displays the name of the program
+ * main - program entry
+ */
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Displays the number of arguments passed to the program
+ * main - program entry
+ */
 int main(int argc, char *argv[])
 {
-    printf("%d\n", argc);
+	printf("%d\n", argc);
 }
