fc5cdb64f431a40181b42cae208a24f6e74d024f
EJAyarma <johnayarmahe@gmail.com>
2022-04-02 02:01:13 +0000

    Add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the product of two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    int x, y, product;
+
+    if (argc - 1 != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    x = atoi(argv[1]);
+    y = atoi(argv[2]);
+    product = x * y;
+
+    printf("%d\n", product);
+    return (0);
+}
