1d7ee47cdfb7a65f61a7b38fc45705b5bd0482e2
EJAyarma <johnayarmahe@gmail.com>
2022-04-13 05:48:07 +0000

    Add task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - Short description
+ * @owner: Owner of dog
+ * @owner: Name of dog
+ * @age: Dog's age
+ *
+ * Description: A simple definition of dog
+ */
 struct dog
 {
 	char *name;
