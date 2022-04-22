7f988420ff736aefbc2773f0406bd2dd3a748914
EJAyarma <johnayarmahe@gmail.com>
2022-04-22 02:52:47 +0000

    Add betty compliance to 1st advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@
  */
 void functionBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
