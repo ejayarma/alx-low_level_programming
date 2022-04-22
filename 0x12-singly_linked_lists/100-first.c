236217f5a737b908db3de0f90ec43efae9d607f6
EJAyarma <johnayarmahe@gmail.com>
2022-04-22 02:51:43 +0000

    Add betty compliance to 1st advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * functionBeforeMain - a function that prints something
+ * before the main function is executed
+ *
+ * Return: void.
+ */
 void functionBeforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
