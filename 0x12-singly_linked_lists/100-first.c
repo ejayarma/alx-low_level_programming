67e87696d7432ba4f70da66b23e2092269dbd41f
EJAyarma <johnayarmahe@gmail.com>
2022-04-22 02:48:06 +0000

    Add first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,3 @@ void functionBeforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
-int main(void)
-{
-	printf("Hello World\n");
-	return (0);
-}
