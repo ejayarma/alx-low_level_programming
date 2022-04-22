a0826710fb51c31c8e0b1240558d260d1ebb766c
EJAyarma <johnayarmahe@gmail.com>
2022-04-22 02:47:34 +0000

    Add first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
-void functionBeforeMain(void) __attribute__((constructor));
-
 void functionBeforeMain(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	printf("Hello World\n");
+	return (0);
 }
