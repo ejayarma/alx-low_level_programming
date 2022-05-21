5ee57ed165a36d7ccbd9dcb10963a62180c009d8
EJAyarma <johnayarmahe@gmail.com>
2022-05-21 23:18:40 +0000

    Another advanced task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,20 +7,24 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			if (i < j)
+			for (k = 48; k < 58; k++)
 			{
-				putchar(i);
-				putchar(j);
-				if (!(i == '8' && j == '9'))
+				if (i < j && j < k)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
