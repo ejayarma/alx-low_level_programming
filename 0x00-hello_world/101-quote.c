156d890bdf9a1c5598834acb50aec3ec5101921a
EJAyarma <johnayarmahe@gmail.com>
2022-05-21 21:53:54 +0000

    Add another advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of program
+ * Return: always 0
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
+
+	while (quote[i])
+	{
+		putchar(quote[i]);
+		i += 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
