9ed13e935d894dcadb38c53957b2f171fcb39c21
EJAyarma <johnayarmahe@gmail.com>
2022-05-21 21:58:44 +0000

    Fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\0";
 	int i = 0;
 
 	while (quote[i])
