f6da5e53b8555d3be660e539d39526eb84cf603d
EJAyarma <johnayarmahe@gmail.com>
2022-05-21 22:07:35 +0000

    fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -9,7 +8,8 @@
 int main(void)
 {
 	size_t len;
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\0";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	len = strlen(quote);
 
 	write(STDIN_FILENO, quote, len);
