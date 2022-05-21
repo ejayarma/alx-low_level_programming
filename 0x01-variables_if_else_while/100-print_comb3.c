5be7c409bd4717b40b5f377f0d866c1dc06377fd
EJAyarma <johnayarmahe@gmail.com>
2022-05-21 23:10:55 +0000

    Add advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point of program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (i < j || j == 0)
+			{
+				putchar(i);
+				putchar(j);
+				if (!(i == '8' && j == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
