76b592441c546b25c1ba89162759101eab9aecb7
EJAyarma <johnayarmahe@gmail.com>
2022-05-23 16:04:45 +0000

    Add new advanced  task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point of program
+ * Return: Returns 0
+ */
+int main(void)
+{
+int i, j, k, l, a, b;
+
+for (i = 48; i < 58; i++)
+{
+	for (j = 48; j < 58; j++)
+	{
+		for (k = 48; k < 58; k++)
+		{
+			for (l = 48; l < 58; l++)
+			{
+				a = (i - 48) * 10 + (j - 48);
+				b = (k - 48) * 10 + (l - 48);
+				if ((j < k || i < l) && (a < b))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == '9' && j == '8' && k == '9' && l == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
+
